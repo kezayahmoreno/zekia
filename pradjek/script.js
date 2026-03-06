4d3e7a2334d1d16f7a755c00b1d608570cc90c61
kezayahmoreno <violanteerinmae@gmail.com>
2026-03-06 08:41:55 +0800

    Add files via upload

---
+++ pradjek/script.js
@@ -0,0 +1,9 @@
+function goToSkills(){
+    document.location.href = "skills.html";
+}
+function goToCompletedWorks(){
+    document.location.href = "completed works.html";
+}
+function goToContact(){
+    document.location.href = "contact.html";
+}
